a608b101161f6a60961717b9ec919f809f33b599
Thomas Morgenstern <tmorgen1@my.westga.edu>
2020-10-13 13:26:38 -0400

    Added a few namespaces and moved some stuff around.
    Got a Patient model class skeleton made.

---
+++ ClinicDatabaseSystem/Model/Patient.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicDatabaseSystem.Model
+{
+    class Patient
+    {
+        public int PatientID { get; set; }
+
+        public string LastName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public DateTime Dob { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Address { get; set; }
+
+        public string Zip { get; set; }
+
+        public Patient(int patientId, string lastName, string firstName, DateTime dob, string phoneNumber, string address, string zip)
+        {
+            this.PatientID = patientId;
+            this.LastName = lastName;
+            this.FirstName = firstName;
+            this.Dob = dob;
+            this.PhoneNumber = phoneNumber;
+            this.Address = address;
+            this.Zip = zip;
+        }
+    }
+}
