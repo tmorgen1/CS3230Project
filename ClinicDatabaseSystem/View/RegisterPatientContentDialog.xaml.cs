d9a225608da900d51f55d91b90c8c2fbc4a8686c
mpavich2 <mpavich2@my.westga.edu>
2020-10-18 17:57:53 -0400

    Added error checking for registering patient. Added error checking for address, address2, zip, city, and state

--- ClinicDatabaseSystem/View/RegisterPatientContentDialog.xaml.cs
+++ ClinicDatabaseSystem/View/RegisterPatientContentDialog.xaml.cs
@@ -90,5 +90,111 @@ namespace ClinicDatabaseSystem.View
                 this.stateComboBox.Items?.Add(state);
             }
         }
+
+        private bool isStateEmpty()
+        {
+            if (this.stateComboBox.SelectedIndex == -1)
+            {
+                this.stateErrorTextBlock.Text = "Must select a state.";
+                this.stateErrorTextBlock.Visibility = Visibility.Visible;
+                return true;
+            }
+
+            return false;
+        }
+
+        private void onCityTextKeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            if (this.cityTextBox.Text.Length == this.cityTextBox.MaxLength)
+            {
+                this.cityErrorTextBlock.Text = "Max character limit reached.";
+                this.cityErrorTextBlock.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                this.cityErrorTextBlock.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void zipTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (this.zipTextBox.Text.Length < 5)
+            {
+                this.zipErrorTextBlock.Text = "Zip must be 5 characters.";
+                this.zipErrorTextBlock.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                this.zipErrorTextBlock.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void zipTextBox_KeyDown(object sender, RoutedEventArgs e)
+        {
+            if (this.zipTextBox.Text.Length == this.zipTextBox.MaxLength)
+            {
+                this.zipErrorTextBlock.Text = "Max character limit reached.";
+                this.zipErrorTextBlock.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                this.zipErrorTextBlock.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void address2TextBox_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            if (this.address2TextBox.Text.Length == this.address2TextBox.MaxLength)
+            {
+                this.address2ErrorTextBlock.Text = "Max character limit reached.";
+                this.address2ErrorTextBlock.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                this.address2ErrorTextBlock.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void addressTextBox_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            if (this.addressTextBox.Text.Length == this.addressTextBox.MaxLength)
+            {
+                this.addressErrorTextBlock.Text = "Max character limit reached.";
+                this.addressErrorTextBlock.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                this.addressErrorTextBlock.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void AddressTextBox_OnLostFocus(object sender, RoutedEventArgs e)
+        {
+            this.addressErrorTextBlock.Visibility = Visibility.Collapsed;
+        }
+
+        private void Address2TextBox_OnLostFocus(object sender, RoutedEventArgs e)
+        {
+            this.address2ErrorTextBlock.Visibility = Visibility.Collapsed;
+        }
+
+        private void CityTextBox_OnLostFocus(object sender, RoutedEventArgs e)
+        {
+            this.cityErrorTextBlock.Visibility = Visibility.Collapsed;
+        }
+
+        private void ZipTextBox_OnBeforeTextChanging(TextBox sender, TextBoxBeforeTextChangingEventArgs args)
+        {
+            if (args.NewText.Any(c => !char.IsDigit(c)))
+            {
+                args.Cancel = true;
+                this.zipErrorTextBlock.Text = "Only Digits allowed.";
+                this.zipErrorTextBlock.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                this.zipErrorTextBlock.Visibility = Visibility.Collapsed;
+            }
+        }
     }
 }
