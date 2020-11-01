c9c86554c5867412443ab313c6e14919cc9c2951
Thomas Morgenstern <tmorgen1@my.westga.edu>
2020-11-01 14:02:40 -0500

    Added some necessary ViewModel class/changes.
    Needed a viewmodel wrapper class to contain some information, also added some patient/doctor DAL functionality.

--- ClinicDatabaseSystem/DAL/DoctorDAL.cs
+++ ClinicDatabaseSystem/DAL/DoctorDAL.cs
@@ -62,6 +62,59 @@ namespace ClinicDatabaseSystem.DAL
             return doctors;
         }
 
+        public static Doctor GetDoctorById(int dId)
+        {
+            using (MySqlConnection conn = DbConnection.GetConnection())
+            {
+                conn.Open();
+                string query = "select * from doctor where doctorID = @dId";
+
+                using (MySqlCommand comm = new MySqlCommand(query, conn))
+                {
+                    comm.Parameters.Add("@dId", MySqlDbType.Int32);
+                    comm.Parameters["@dId"].Value = dId;
+
+                    using (MySqlDataReader reader = comm.ExecuteReader())
+                    {
+                        int idOrdinal = reader.GetOrdinal("doctorID");
+                        int lastNameOrdinal = reader.GetOrdinal("lastName");
+                        int firstNameOrdinal = reader.GetOrdinal("firstName");
+                        int dobOrdinal = reader.GetOrdinal("dob");
+                        int phoneNumberOrdinal = reader.GetOrdinal("phoneNumber");
+                        int addressOrdinal = reader.GetOrdinal("address");
+                        int zipOrdinal = reader.GetOrdinal("zip");
+
+                        while (reader.Read())
+                        {
+                            int id = !reader.IsDBNull(idOrdinal) ? reader.GetInt32(idOrdinal) : 0;
+                            string lastName = !reader.IsDBNull(lastNameOrdinal)
+                                ? reader.GetString(lastNameOrdinal)
+                                : null;
+                            string firstName = !reader.IsDBNull(firstNameOrdinal)
+                                ? reader.GetString(firstNameOrdinal)
+                                : null;
+                            DateTime dob = !reader.IsDBNull(dobOrdinal)
+                                ? reader.GetDateTime(dobOrdinal)
+                                : default(DateTime);
+                            string phoneNumber = !reader.IsDBNull(phoneNumberOrdinal)
+                                ? reader.GetString(phoneNumberOrdinal)
+                                : null;
+                            string address1 = !reader.IsDBNull(addressOrdinal)
+                                ? reader.GetString(addressOrdinal)
+                                : null;
+                            string zip = !reader.IsDBNull(zipOrdinal) ? reader.GetString(zipOrdinal) : null;
+                            Address address = GetAddressAndZipNewConnection(id, address1, zip);
+                            IList<DoctorSpecialty> specialties = GetSpecialtiesNewConnection(id);
+
+                            return new Doctor(id, lastName, firstName, dob, phoneNumber, address, specialties);
+                        }
+                    }
+                }
+            }
+
+            return null;
+        }
+
         private static Address GetAddressAndZipNewConnection(int doctorId, string address1, string zip)
         {
             return AddressDAL.GetAddressWithDoctorId(doctorId, address1, zip);
