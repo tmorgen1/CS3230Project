3c7027c7fbbefbda41a3094144ef4991f49480dd
Michael Pavich <mpavich2@my.westga.edu>
2020-11-14 20:55:05 -0500

    Added UI for both tests views

---
+++ ClinicDatabaseSystem/View/OrderTestContentDialog.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Content Dialog item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace ClinicDatabaseSystem.View
+{
+    public sealed partial class OrderTestContentDialog : ContentDialog
+    {
+        public OrderTestContentDialog()
+        {
+            this.InitializeComponent();
+        }
+
+        private void cancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void orderButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void addButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
