57d8108908cdd40e1f2d5a7b5b257988e153cdbd
Thomas Morgenstern <tmorgen1@my.westga.edu>
2020-11-15 16:12:46 -0500

    Updated TestResultDAL.
    Now allows editing test result in database.

--- ClinicDatabaseSystem/DAL/TestResultDAL.cs
+++ ClinicDatabaseSystem/DAL/TestResultDAL.cs
@@ -73,5 +73,36 @@ namespace ClinicDatabaseSystem.DAL
                 }
             }
         }
+
+        public static bool EditTestResult(TestResult newTestResult, TestResult oldTestResult)
+        {
+            using (MySqlConnection conn = DbConnection.GetConnection())
+            {
+                conn.Open();
+                string editStatement = "update test_result set testID = @tId, patientID = @pId, dateTime = @dateTime," +
+                                       " results = @results where testID = @oldTId and patientID = @oldPId and dateTime = @oldDateTime";
+
+
+                using (MySqlCommand comm = new MySqlCommand(editStatement, conn))
+                {
+                    comm.Parameters.Add("@tId", MySqlDbType.Int32);
+                    comm.Parameters["@tId"].Value = newTestResult.TestId;
+                    comm.Parameters.Add("@pId", MySqlDbType.Int32);
+                    comm.Parameters["@pId"].Value = newTestResult.PatientId;
+                    comm.Parameters.Add("@dateTime", MySqlDbType.DateTime);
+                    comm.Parameters["@dateTime"].Value = newTestResult.ResultDateTime;
+                    comm.Parameters.Add("@results", MySqlDbType.VarChar);
+                    comm.Parameters["@results"].Value = newTestResult.Results;
+                    comm.Parameters.Add("@oldTId", MySqlDbType.Int32);
+                    comm.Parameters["@oldTId"].Value = oldTestResult.TestId;
+                    comm.Parameters.Add("@oldPId", MySqlDbType.Int32);
+                    comm.Parameters["@oldPId"].Value = oldTestResult.PatientId;
+                    comm.Parameters.Add("@oldDateTime", MySqlDbType.DateTime);
+                    comm.Parameters["@oldDateTime"].Value = oldTestResult.ResultDateTime;
+
+                    return comm.ExecuteNonQuery() > 0;
+                }
+            }
+        }
     }
 }
